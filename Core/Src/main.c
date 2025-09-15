af93dbd0c89eee8bfeda6316a86631f9289f7ccc
sy <sunnywy12@naver.com>
2025-09-15 22:17:52 +0900

    interrupt

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -82,13 +82,6 @@ uint8_t payload[6];
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-void MX_GPIO_Init(void);
-void MX_DMA_Init(void);
-void MX_ADC1_Init(void);
-void MX_SPI1_Init(void);
-void MX_USART2_UART_Init(void);
-void MX_TIM2_Init(void);
-
 /* USER CODE BEGIN PFP */
 /* USER CODE END PFP */
 
